b816b27e3a5c58a98148c5d67f664a0d95edd52f
Thibaud CASTAING <t-cas@users.noreply.github.com>
2017-07-25 01:10:15 +0200

    reduce logging level of some logs

--- jumpssh/session.py
+++ jumpssh/session.py
@@ -181,7 +181,7 @@ class SSHSession(object):
             for remote_session in self.ssh_remote_sessions.values():
                 remote_session.close()
         if hasattr(self, 'ssh_client') and self.is_active():
-            logger.info("Closing connection to '%s:%s'..." % (self.host, self.port))
+            logger.debug("Closing connection to '%s:%s'..." % (self.host, self.port))
             self.ssh_client.close()
             # clear local host keys as they may not be valid for next connection
             self.ssh_client.get_host_keys().clear()
@@ -301,9 +301,6 @@ class SSHSession(object):
                     raise exception.TimeoutError(
                         "Timeout of %ds reached when calling command '%s'. "
                         "Increase timeout if you think the command was still running successfully." % (timeout, cmd))
-
-        if not silent:
-            logger.debug(output.getvalue())
 
         exit_code = channel.recv_exit_status()
         output_value = output.getvalue().strip()
