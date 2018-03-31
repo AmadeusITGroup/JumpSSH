6356b020261ccda7b94fdeefc075df1768e1f871
Florian MAUPAS <florian.maupas@amadeus.com>
2018-03-31 15:49:40 -0400

    Allows to put a file even if no command on remote host has been performed yet

--- jumpssh/session.py
+++ jumpssh/session.py
@@ -581,6 +581,9 @@ class SSHSession(object):
         logger.debug("Copy local file '%s' on remote host '%s' in '%s' as '%s'"
                      % (local_path, self.host, remote_path, self.username))
 
+        # ensure the connection is open
+        self.open()
+	
         # create file remotely
         with open(local_path, 'rb') as local_file:
             self.file(remote_path=remote_path, content=local_file.read(),
